ad6421f06f70413833cf773268d31e445af09ae8
Victor Freire <victor.freire@mustti.com.br>
2019-06-27 08:41:29 -0300

    organized config

--- config.py
+++ config.py
@@ -1,17 +1,14 @@
 import os
 from pathlib import Path
 
+# expiration in minutes
+JWT_EXP = (1400) * 60
 JWT_SECRET = os.getenv('JWT_SECRET', '123')
 JWT_ALGORITHM = 'HS256'
 
-# expiration in minutes
-JWT_EXP = (1400) * 60
-
 
 DATABASE_DIR = Path(os.path.dirname(__file__)) / 'bin' / 'database'
-
-if not os.path.exists(DATABASE_DIR):
-    os.makedirs(DATABASE_DIR)
-
 DATABASE_URL = 'sqlite:///%s/sqlite3.db' % (DATABASE_DIR)
 DATABASE_ECHO = False
+if not os.path.exists(DATABASE_DIR):
+    os.makedirs(DATABASE_DIR)
